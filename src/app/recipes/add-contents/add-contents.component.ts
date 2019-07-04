0a424e5bbe75e3bc8b6d889e5f233ad79a82f578
Ivo Valkov <joomfxdesign@gmail.com>
2019-07-04 20:48:04 +0300

    Initial work on the Search/Filtering functionality in the AddContents component.

--- src/app/recipes/add-contents/add-contents.component.ts
+++ src/app/recipes/add-contents/add-contents.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { ProductsDataService } from '../services/products-data.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Products } from '../../../app/common/interfaces/products';
 import { Product } from '../../../app/common/interfaces/product';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contents',
@@ -10,34 +11,34 @@ import { Product } from '../../../app/common/interfaces/product';
   styleUrls: ['./add-contents.component.css']
 })
 export class AddContentsComponent implements OnInit {
+  public filterForm: FormGroup;
   public products: Product[] = [];
 
+  @Input() public addedProductsModal: Product[];
   @Output() public addedProduct = new EventEmitter<Product>();
-
-  // Maybe delete?
-  public addedProductsModal: Product[] = [];
 
   constructor(
     private readonly productDataService: ProductsDataService,
     private readonly modalService: NgbModal,
+    private readonly formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
+    this.filterForm = this.formBuilder.group({
+      search: [''],
+    });
+
     this.productDataService.getProducts().subscribe(
       (products: Products) => this.products = products.products
     );
   }
 
   public open(modalWindow): void {
-    this.modalService.open(modalWindow);
+    this.modalService.open(modalWindow, { size: 'lg' });
   }
 
   public addProductToRecipe(product) {
-    // console.log(product);
     this.addedProduct.emit(product);
-
-    // Maybe delete?
-    this.addedProductsModal.push(product);
   }
 
 }
