2278cd2a605c61c3421152eab078d9484a48c75c
rostislav <rostislavterziev@gmail.com>
2019-07-19 14:58:36 +0300

    created user details service and user interface

---
+++ src/app/users/services/users-data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../../../app/common/interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersDataService {
+
+  constructor(
+    private readonly http: HttpClient,
+  ) { }
+
+  public getSingleUser(username: string): Observable<User> {
+    return this.http.get<User>(`http://localhost:3000/api/users/${username}`);
+  }
+}
